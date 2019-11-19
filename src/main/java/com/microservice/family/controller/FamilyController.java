f9a5746376dd2b8de09dc0555eea383e86204214
Jose Alejandro <paredes_191@hotmail.com>
2019-11-19 17:40:24 -0500

    add CRUD

--- src/main/java/com/microservice/family/controller/FamilyController.java
+++ src/main/java/com/microservice/family/controller/FamilyController.java
@@ -1,11 +1,20 @@
 package com.microservice.family.controller;
 
 import com.microservice.family.component.FamilyConverter;
+import com.microservice.family.model.dto.FamilyDto;
 import com.microservice.family.service.FamilyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @RestController
+@RequestMapping("/api/family")
 public class FamilyController {
 
   @Autowired
@@ -13,5 +22,44 @@ public class FamilyController {
 
   @Autowired
   private FamilyConverter familyConverter;
+
+  @GetMapping
+  public Mono<ResponseEntity<Flux<FamilyDto>>> listar(){
+    return Mono.just(ResponseEntity
+            .ok().contentType(MediaType.APPLICATION_JSON).body(familyService.findAll().flatMap(
+                    family -> Mono.just(familyConverter.converToDto(family))
+            )));
+  }
+
+  @GetMapping("/{id}")
+  public Mono<ResponseEntity<FamilyDto>> ver(@PathVariable String id){
+    return familyService.findById(id)
+            .flatMap(family -> Mono.just(familyConverter.converToDto(family)))
+            .map(family -> ResponseEntity
+                    .ok().contentType(MediaType.APPLICATION_JSON).body(family));
+  }
+
+  @PostMapping
+  public  Mono<ResponseEntity<FamilyDto>> registrar(@RequestBody FamilyDto family){
+    return familyService.create(familyConverter.convertToDocument(family))
+            .flatMap(f -> Mono.just(familyConverter.converToDto(f)))
+            .map(f -> ResponseEntity
+                    .created(URI.create("/api/family")).contentType(MediaType.APPLICATION_JSON).body(f));
+  }
+
+  @PutMapping("/{id}")
+  public Mono<ResponseEntity<FamilyDto>> actulizar(@RequestBody FamilyDto family, @PathVariable String id){
+    return familyService.update(familyConverter.convertToDocument(family), id)
+            .flatMap(f -> Mono.just(familyConverter.converToDto(f)))
+            .map(f -> ResponseEntity
+                    .created(URI.create("/api/family")).contentType(MediaType.APPLICATION_JSON).body(f));
+  }
+
+  @DeleteMapping("/{id}")
+  public Mono<ResponseEntity<Void>> eliminar(@PathVariable String id){
+    return familyService.delete(id)
+            .flatMap(p -> Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)))
+            .defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
+  }
 
 }
--- src/main/java/com/microservice/family/service/impl/FamilyServiceImpl.java
+++ src/main/java/com/microservice/family/service/impl/FamilyServiceImpl.java
@@ -1,35 +1,48 @@
 package com.microservice.family.service.impl;
 
 import com.microservice.family.model.document.Family;
+import com.microservice.family.respository.FamilyRepository;
 import com.microservice.family.service.FamilyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class FamilyServiceImpl implements FamilyService {
+
+  @Autowired
+  private FamilyRepository familyRepository;
+
   @Override
   public Flux<Family> findAll() {
-    return null;
+    return familyRepository.findAll();
   }
 
   @Override
   public Mono<Family> findById(String id) {
-    return null;
+    return familyRepository.findById(id);
   }
 
   @Override
   public Mono<Family> create(Family family) {
-    return null;
+    return familyRepository.save(family);
   }
 
   @Override
   public Mono<Family> update(Family family, String id) {
-    return null;
+    return findById(id).flatMap(f -> {
+      f.setFullName(family.getFullName());
+      f.setBirthdate(family.getBirthdate());
+      f.setGender(family.getGender());
+      f.setTypeDocument(family.getTypeDocument());
+      f.setNumberDocument(family.getNumberDocument());
+      return familyRepository.save(f);
+    });
   }
 
   @Override
   public Mono<Void> delete(String id) {
-    return null;
+    return findById(id).flatMap(f -> familyRepository.delete(f));
   }
 }
