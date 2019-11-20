e77fc0caeb1e3b11a55ff28808a63c3bf2e87663
Jose Alejandro <paredes_191@hotmail.com>
2019-11-20 15:24:43 -0500

    Fix: Repository

--- src/main/java/com/microservice/family/respository/FamilyRepository.java
+++ src/main/java/com/microservice/family/respository/FamilyRepository.java
@@ -1,6 +1,7 @@
 package com.microservice.family.respository;
 
+import com.microservice.family.model.document.Family;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface FamilyRepository extends ReactiveMongoRepository {
+public interface FamilyRepository extends ReactiveMongoRepository<Family, String> {
 }
