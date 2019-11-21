99bd08b8ee0648cce66ca7674292182870d8d6fd
Jose Alejandro <paredes_191@hotmail.com>
2019-11-20 22:11:00 -0500

    rename names for method controllers

--- src/main/java/com/microservice/family/controller/FamilyController.java
+++ src/main/java/com/microservice/family/controller/FamilyController.java
@@ -21,44 +21,42 @@ public class FamilyController {
   private FamilyService familyService;
 
   @GetMapping
-  public Mono<ResponseEntity<Flux<FamilyDto>>> listar(){
+  public Mono<ResponseEntity<Flux<FamilyDto>>> findAll(){
     return Mono.just(ResponseEntity
             .ok().contentType(MediaType.APPLICATION_JSON).body(familyService.findAll()));
   }
 
   @GetMapping("/{id}")
-  public Mono<ResponseEntity<FamilyDto>> ver(@PathVariable String id){
+  public Mono<ResponseEntity<FamilyDto>> findById(@PathVariable String id){
     return familyService.findById(id)
             .map(family -> ResponseEntity
                     .ok().contentType(MediaType.APPLICATION_JSON).body(family));
   }
 
   @GetMapping("/student/{idStudent}")
-  public Flux<ResponseEntity<FamilyDto>> findById(@PathVariable String idStudent){
-    return familyService.findByIdStudent(idStudent)
-            .map(family -> ResponseEntity
-                    .ok().contentType(MediaType.APPLICATION_JSON).body(family));
+  public Mono<ResponseEntity<Flux<FamilyDto>>> findByIdStudent(@PathVariable String idStudent){
+    return Mono.just(ResponseEntity.ok()
+            .contentType(MediaType.APPLICATION_JSON).body(familyService.findByIdStudent(idStudent)));
   }
 
   @PostMapping
-  public  Mono<ResponseEntity<FamilyDto>> registrar(@RequestBody FamilyDto family){
+  public  Mono<ResponseEntity<FamilyDto>> save(@RequestBody FamilyDto family){
     return familyService.create(family)
             .map(f -> ResponseEntity
                     .created(URI.create("/api/family")).contentType(MediaType.APPLICATION_JSON).body(f));
   }
 
   @PutMapping("/{id}")
-  public Mono<ResponseEntity<FamilyDto>> actulizar(@RequestBody FamilyDto family, @PathVariable String id){
+  public Mono<ResponseEntity<FamilyDto>> update(@RequestBody FamilyDto family, @PathVariable String id){
     return familyService.update(family, id)
             .map(f -> ResponseEntity
                     .created(URI.create("/api/family")).contentType(MediaType.APPLICATION_JSON).body(f));
   }
 
   @DeleteMapping("/{id}")
-  public Mono<ResponseEntity<Void>> eliminar(@PathVariable String id){
+  public Mono<ResponseEntity<Void>> delete(@PathVariable String id){
     return familyService.delete(id)
-            .flatMap(p -> Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)))
-            .defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
+            .flatMap(p -> Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)));
   }
 
 }
