e7935e8292dc196e90717374ea8344d290e79ae3
Jose Alejandro <paredes_191@hotmail.com>
2019-11-22 16:00:50 -0500

    add test

--- src/test/java/com/microservice/family/FamilyApplicationTests.java
+++ src/test/java/com/microservice/family/FamilyApplicationTests.java
@@ -1,13 +1,42 @@
 package com.microservice.family;
 
-import org.junit.jupiter.api.Test;
+import com.microservice.family.model.dto.FamilyDto;
+import com.microservice.family.model.dto.StudentDto;
+import com.microservice.family.service.FamilyService;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class FamilyApplicationTests {
 
+	@Autowired
+	private WebTestClient testClient;
+
+	@Autowired
+	private FamilyService familyService;
+
 	@Test
-	void contextLoads() {
+	public void getAllTest() {
+		testClient.get()
+						.uri("api/family")
+						.accept(MediaType.APPLICATION_JSON)
+						.exchange()
+						.expectStatus().isOk()
+						.expectHeader().contentType(MediaType.APPLICATION_JSON)
+						.expectBodyList(FamilyDto.class)
+						.consumeWith(response -> {
+							List<FamilyDto> familys = response.getResponseBody();
+							familys.forEach(family -> System.out.println(family.getFullName()));
+						});
 	}
 
 }
