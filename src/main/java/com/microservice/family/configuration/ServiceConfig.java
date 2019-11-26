3b3a08e431c564442212f9a9be8e369faa6241a3
Jose Alejandro <paredes_191@hotmail.com>
2019-11-26 09:30:55 -0500

    change path service

--- src/main/java/com/microservice/family/configuration/ServiceConfig.java
+++ src/main/java/com/microservice/family/configuration/ServiceConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class ServiceConfig {
 
-  @Value("${config.base.student.uri}")
+  @Value("${config.base.service.uri}")
   private String pathStudent;
 
   @Bean
